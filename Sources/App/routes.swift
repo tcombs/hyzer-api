d40b4d6d2fa605463ba5af12a60795017c312ce6
Tyler Combs <tyler@tlcombs.com>
2022-01-26 17:27:51 -0600

    deploy test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return "It works from github!"
     }
 
     app.get("hello") { req -> String in
